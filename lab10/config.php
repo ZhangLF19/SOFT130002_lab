a5ce03a1ac38bc36ad53098782a72651ec08bea7
ZhangLF <19302010052@fudan.edu.cn>
2020-06-07 17:54:48 +0800

    lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'root');
+define('DBPASS', '20010329fF');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
